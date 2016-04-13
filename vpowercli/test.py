631942dde6cc9754f084b329d2154b1b9bfe8e9f
Hector Vargas <vargash1@wit.edu>
2016-04-12 22:56:58 -0400

    :coffee: WIndows sucks

---
+++ vpowercli/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Vargas Hector <vargash1>
+# @Date:   Tuesday, April 12th 2016, 10:54:06 pm
+# @Email:  vargash1@wit.edu
+# @Last modified by:   vargash1
+# @Last modified time: Tuesday, April 12th 2016, 10:56:25 pm
+import unicurses
+
+stdscr = unicurses.initscr()
