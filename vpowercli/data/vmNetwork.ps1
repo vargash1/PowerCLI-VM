92a9fbdd06a2e1bf899715e94ac5982fe7eede42
Hector Vargas <vargash1@wit.edu>
2016-04-15 12:52:21 -0400

    Pls work

--- vpowercli/data/vmNetwork.ps1
+++ vpowercli/data/vmNetwork.ps1
@@ -10,10 +10,9 @@ $newip = Read-Host -Prompt 'Input new ip for vm'
 $newnetmask = '255.255.255.240'
 $newgateway = '69.43.73.209'
 $ndns = '69.43.73.211'
-$vmnetinterface = Get-VMGuestNetworkInterface -VM $config
 
 $specName = "tempSpec" + (Get-Random)
-Get-OSCustomizationSpec -Name $spec | New-OSCustomizationSpec -Name $specName -Type NonPersistent
+Get-OSCustomizationSpec -Name "Window"| New-OSCustomizationSpec -Name $specName -Type NonPersistent
 
 Get-OSCustomizationSpec -Name $specName | Get-OSCustomizationNicMapping | Set-OSCustomizationNicMapping `
 -IpMode UseStaticIP -IpAddress $newip -SubnetMask $newnetmask -DefaultGateway $newgateway -Dns $ndns
@@ -22,7 +21,7 @@ $tempSpec = Get-OSCustomizationSpec -Name $specName
 
 $vmhost = (Get-Cluster | Get-VMHost | Sort-Object -Property MemoryUsageGB | Select-Object -First 1)
 
-#Create the VM
+#Create the `VM
 New-VM -Name $vmname -template $template -OsCustomizationSpec $tempSpec -VMHost $hostname -Confirm:$true
 
 #Cleanup the temporary Spec. System will do this outside of the session, but this will allow the scripts to be reused within a session.
