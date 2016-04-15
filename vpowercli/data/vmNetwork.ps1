80777ac5e7accfb308418ea819b0bf02bbcbbfd8
Hector Vargas <vargash1@wit.edu>
2016-04-15 12:22:56 -0400

    :coffee: We presenting

--- vpowercli/data/vmNetwork.ps1
+++ vpowercli/data/vmNetwork.ps1
@@ -2,12 +2,16 @@ Add-PSSnapin Vmware.VimAutomation.Core
 $adminpasswd = Read-Host -Prompt 'Input your teame server passwd'
 Connect-VIServer -Server vc.teame.witcsn.net -Protocol https -User 'Administrator@vsphere.local' -Password $adminpasswd
 
-$vmname = Read-Host -Prompt 'Input the vm name that you wish to change'
+$vmname = Read-Host -Prompt 'Input the new vm name that you wish to spawn'
+$hostname = Read-Host -Prompt 'Enter the host(h2,h3,h4)'
+$datastorename = Read-Host -Prompt 'Enter the datastore to use'
 $vmconfig = Get-VM -Name $vmname
 
 $newip = Read-Host -Prompt 'Input new ip for vm'
 $newnetmask = '255.255.255.240'
 $newgateway = '69.43.73.209'
+$ndns = '69.43.73.211'
 $vmnetinterface = Get-VMGuestNetworkInterface -VM $config
 
-Set-VMGuestNetworkInterface -VMGuestNetworkInterface $vmnetinterface -Netmask $newnetmask -Gateway $newgateway -Ip $newip
+Set-OSCustomizationNicMapping -IpMode UseStaticIp -IpAddress $newip -SubnetMask $newnetmask  -DefaultGateway $newgateway -Dns $ndns
+New-VM -Name $vmname -Template 'powercli' -VMHost $hostname -Datastore $datastorename -RunAsync
